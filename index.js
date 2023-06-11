b007865a2dac6cc947877cbe5be63f7a062313c4
Paul Jones <pajones@adobe.com>
2023-06-10 18:09:27 -0600

    stage is working

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ import { Router } from 'itty-router';
 // Create a new router
 const router = Router();
 
-const corsHeaders = {
-		'Access-Control-Allow-Origin': 'https://games.chill.ws',
+const corsHeaders = (env) => ({
+		'Access-Control-Allow-Origin': env.STAGE ? 'https://stage.games-ui.pages.dev' : 'https://games.chill.ws',
 		'Access-Control-Allow-Headers': 'x-cfp, content-type',
 		'Access-Control-Allow-Methods': 'GET, PUT, OPTIONS',
-		'Access-Control-Allow-Credentials': true,
-};
+		'Access-Control-Allow-Credentials': 'true',
+});
 
 router.get('/games/list', async (request, env, context) => {
-		if (request.headers.get('x-cfp') != env.CFP_PASSWORD) {
-				return new Response(JSON.stringify([{"id":"error","name":"Error retrieving list of games","cover":"/images/error.jpg"}]), {status: 200, headers: {...corsHeaders,}});
-		}
+	if (request.headers.get('x-cfp') != env.CFP_PASSWORD) {
+			return new Response(JSON.stringify([{"id":"error","name":"Error retrieving list of games","cover":"/images/error.jpg"}]), {status: 200, headers: {...corsHeaders(env),}});
+	}
 
-		let keys = await env.games.list();
+	let keys = await env.games.list();
 
-		let json = await Promise.all(keys.keys.map(key => env.games.get(key.name, { type: "json" })))
-				.then(values => JSON.stringify(values));
-		return new Response(json, {
-				status: 200,
-				headers: {
-						...corsHeaders,
-						"Content-type": "application/json",
-				}
-		});
+	let json = await Promise.all(keys.keys.map(key => env.games.get(key.name, { type: "json" })))
+			.then(values => JSON.stringify(values));
+	return new Response(json, {
+			status: 200,
+			headers: {
+					...corsHeaders(env),
+					"Content-type": "application/json",
+			}
+	});
 });
 
 /*
@@ -58,7 +58,7 @@ router.put('/games/borrow', async (request, env, context) => {
 		return new Response(json, {
 				status: 200,
 				headers: {
-						...corsHeaders,
+						...corsHeaders(env),
 						"Content-type": "application/json"
 				}
 		});
@@ -87,7 +87,7 @@ router.put('/games/return', async (request, env, context) => {
 		return new Response(json, {
 				status: 200,
 				headers: {
-						...corsHeaders,
+						...corsHeaders(env),
 						"Content-type": "application/json"
 				}
 		});
@@ -100,10 +100,10 @@ above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, 
 Visit any page that doesn't exist (e.g. /foobar) to see it in action.
 */
 
-export const withCorsPreflight = (request) => {
+export const withCorsPreflight = (request, env, context) => {
 		if (request.method.toLowerCase() === 'options') {
 				return new Response('ok', {
-						headers: corsHeaders,
+						headers: corsHeaders(env),
 				});
 		}
 };
